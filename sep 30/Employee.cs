37f458746fd693e16bc21a66d9093e88a1b8abb4
lasyapriyasankuri <lasyapriyasankuri@gmail.com>
2024-10-16 17:21:55 +0530

    Add files via upload

---
+++ sep 30/Employee.cs
@@ -0,0 +1,20 @@
+﻿// Models/Employee.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace Permanent_Contract_Employee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } // Primary Key
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string EmployeeType { get; set; } // Permanent or Contractual
+
+        public decimal Bonus { get; set; } = 0; // Default value
+
+        public decimal Pay { get; set; } = 0;   // Default value
+    }
+}
---
+++ sep 30/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿// Controllers/EmployeeController.cs
+using Microsoft.AspNetCore.Mvc;
+using Permanent_Contract_Employee.Models;
+using Permanent_Contract_Employee.Services;
+
+namespace Permanent_Contract_Employee.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeService _employeeService;
+
+        public EmployeeController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee([FromBody] Employee employee)
+        {
+            if (employee == null) return BadRequest("Employee cannot be null");
+
+            var savedEmployee = _employeeService.AddEmployee(employee);
+            return Ok(savedEmployee);
+        }
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            var employees = _employeeService.GetEmployees();
+            return Ok(employees);
+        }
+    }
+}
