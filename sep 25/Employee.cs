5124b6885714073ef7c4ea03e8ab734c787d5d5c
lasyapriyasankuri <lasyapriyasankuri@gmail.com>
2024-10-16 17:09:25 +0530

    Add files via upload

---
+++ sep 25/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace MVC.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public string FullName => $"{FirstName} {MiddleName} {LastName} ";
+    }
+
+}
---
+++ sep 25/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+// Set Employee as the default controller
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Employee}/{action=Index}/{id?}");
+
+app.Run();
