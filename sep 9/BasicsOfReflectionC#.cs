de1ef5b015350bf7437798d83dc27b904f50a3fc
lasyapriyasankuri <lasyapriyasankuri@gmail.com>
2024-10-16 10:16:49 +0530

    Add files via upload

---
+++ sep 9/BasicsOfReflectionC#.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo
+{
+    public class AssemblyExplorer
+    {
+        public void ExploreAssembly(Assembly assembly)
+        {
+            Console.WriteLine($"Exploring assembly: {assembly.FullName}");
+
+            foreach (var type in assembly.GetTypes())
+            {
+                Console.WriteLine($"Type: {type.Name}");
+                Console.WriteLine($"Base Type: {type.BaseType?.Name}");
+                Console.WriteLine("No Interface Implemented ");
+                Console.WriteLine("------------------------");
+                foreach (var interfaceType in type.GetInterfaces())
+                {
+                    Console.WriteLine($"  {interfaceType.Name}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var explorer = new AssemblyExplorer();
+            explorer.ExploreAssembly(typeof(Program).Assembly);
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ sep 9/RuntimeTypeInspector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Company { get; set; }
+
+    public Person(string name, int age, string address)
+    {
+        Name = name;
+        Age = age;
+        Company = address; 
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person person = new Person("Sai Kumar", 23, "Wipro");
+        Type type = typeof(Person);
+        PropertyInfo[] properties = type.GetProperties();
+        Console.WriteLine("Details of Persons : ");
+        foreach (PropertyInfo property in properties)
+        {
+            Console.WriteLine($"Property: {property.PropertyType}, Value: {property.GetValue(person)}");
+        }
+
+        FieldInfo[] fields = type.GetFields();
+
+       // Console.WriteLine("\nPublic Fields: ");
+        foreach (FieldInfo field in fields)
+        {
+            Console.WriteLine($"Name: {field.Name}, Value: {field.GetValue(person)}");
+        }
+    }
+}
